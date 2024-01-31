4f73c36dbb2041a035ab4d67918b43f623075e09
Kimbix <alemanhumberto106@gmail.com>
2024-01-30 23:05:40 -0400

    Added gitsign keybinds

--- lua/plugins/git-signs.lua
+++ lua/plugins/git-signs.lua
@@ -3,16 +3,42 @@ return {
   config = function()
     require("gitsigns").setup()
 
+    local gs = require("gitsigns")
+    gs.toggle_numhl()
     local wk = require("which-key")
     wk.register({
       g = {
         name = "Git",
 
-        p = { "<CMD>Gitsigns preview_hunk<CR>", "Preview Hunk" },
-        P = { "<CMD>Gitsigns preview_hunk_inline<CR>", "Preview Hunk" },
+        -- Navigation
+        n = { gs.next_hunk, "Go to Next Hunk" },
+        N = { gs.prev_hunk, "Go to Previous Hunk" },
+        v = { gs.select_hunk, "Select Current Hunk" },
 
-        b = { "<CMD>Gitsigns toggle_current_line_blame<CR>", "Toggle Current Line Blame" },
+        -- Hunk Management
+        s = { gs.stage_hunk, "Stage Hunk" },
+        S = { gs.stage_buffer, "Stage Buffer" },
+        u = { gs.undo_stage_hunk, "Undo Stage Hunk" },
+        R = { gs.reset_hunk, "Reset Hunk" },
 
+        -- Preview
+        p = { gs.preview_hunk, "Preview Hunk" },
+        P = { gs.preview_hunk_inline, "Preview Hunk in File" },
+
+        -- View
+        h = { gs.toggle_linehl, "Toggle Line Highlight" },
+        d = { gs.toggle_deleted, "Toggle Deleted Hunks" },
+        w = { gs.toggle_word_diff, "Toggle Word Diff" },
+
+        -- Diff
+        D = { "<CMD>Neotree close<CR> <CMD>Gitsigns diffthis<CR>", "Diff File" },
+
+        b = {
+          function()
+            gs.blame_line({ full = true })
+          end,
+          "Get Blame for Hunk",
+        },
       },
     }, {
       prefix = "<leader>",

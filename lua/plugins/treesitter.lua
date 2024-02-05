be5f6791b27f623120200977d7754140951eed0c
Kimbix <alemanhumberto106@gmail.com>
2024-02-05 13:47:41 -0400

    Treesitter ensures csharp installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "lua", "cpp", "javascript" },
+			ensure_installed = { "lua", "cpp", "javascript", "c_sharp" },
 			highlight = { enable = true },
 			indent = { enable = true },
 		})

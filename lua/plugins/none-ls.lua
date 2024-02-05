7ffca09c8836369817c6ff572c49c5f5b7e4a9a0
Kimbix <alemanhumberto106@gmail.com>
2024-02-05 13:47:33 -0400

    Changed NoneLS Config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,22 +1,24 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
         -- General
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.eslint_d,
         -- Lua
-				null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.stylua,
         -- Python
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
         -- GodotScript
-				null_ls.builtins.formatting.gdformat,
-				null_ls.builtins.diagnostics.gdlint,
-			},
-		})
-		vim.keymap.set("n", "<leader>lF", vim.lsp.buf.format, {})
-	end,
+        null_ls.builtins.formatting.gdformat,
+        null_ls.builtins.diagnostics.gdlint,
+        -- CSharp
+        null_ls.builtins.formatting.clang_format,
+      },
+    })
+    vim.keymap.set("n", "<leader>lF", vim.lsp.buf.format, {})
+  end,
 }

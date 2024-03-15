cc961feade3db83559c3b29f19322003bf228e0b
Kimbix <alemanhumberto106@gmail.com>
2024-03-15 04:59:35 -0400

    Comment.nvim lazy loading

--- lua/plugins/commenter.lua
+++ lua/plugins/commenter.lua
@@ -1,27 +1,19 @@
--- TODO: Setup keybinds for this shit
 return {
 	"numToStr/Comment.nvim",
 	lazy = true,
-	keys = { "<leader>cc", "<leader>cC" },
+	keys = {
+		{ "<leader>cc", ":lua require('Comment.api').toggle.linewise.current", mode = { "n" }, desc = "Toggle current line as comment" },
+		{ "<leader>cC", ":lua require('Comment.api').toggle.blockwise.current", mode = { "n"}, desc = "Toggle current line as comment block" },
+		{ "<leader>cc", "<Plug>(comment_toggle_linewise_visual)", mode = { "v" }, desc = "Toggle current line as comment" },
+		{ "<leader>cC", "<Plug>(comment_toggle_blockwise_visual)", mode = { "v"}, desc = "Toggle current line as comment block" },
+	},
 
-	init = function() -- Keymaps gets registered, but plugin not started, pro
-		local api = require("Comment.api")
-		local wk = require("which-key")
-		wk.register({
-			c = {
-				name = "Comment",
-				c = { api.toggle.linewise.current, "Toggles current line as a comment" },
-				C = { api.toggle.blockwise.current, "Toggles current block as a comment" },
-			},
-		}, { mode = "n", prefix = "<leader>", })
-		wk.register({
-			c = {
-				name = "Comment",
-				c = { "<Plug>(comment_toggle_linewise_visual)", "Toggles current line as a comment" },
-				C = { "<Plug>(comment_toggle_blockwise_visual)", "Toggles current block as a comment" },
-			},
-		}, { mode = "x", prefix = "<leader>", })
+	init = function()
+		require("which-key").register({
+			c = { name = "Comment", },
+		}, { mode = { "n", "v" }, prefix = "<leader>", })
 	end,
+
 	opts = {
 		-- Default settings are mostly fine, but if you want to know the defaults, see them at https://github.com/numToStr/Comment.nvim
 		mappings = false, -- Don't create mappings, let me do it

0d3693b01051af4a03a81f2cace704952721cc84
Kimbix <alemanhumberto106@gmail.com>
2024-03-15 05:16:12 -0400

    GitSigns Lazy loading

--- lua/plugins/git-signs.lua
+++ lua/plugins/git-signs.lua
@@ -2,51 +2,47 @@ return {
   "lewis6991/gitsigns.nvim",
 	name = "gitsigns.nvim",
 	lazy = true,
-  init = function()
+	keys = {
+		-- Movement
+		{ "<leader>gj", ":lua require('gitsigns').next_hunk()<cr>", mode = { "n" }, desc = "Go to Next Hunk" },
+		{ "<leader>gk", ":lua require('gitsigns').prev_hunk()<cr>", mode = { "n" }, desc = "Go to Previous Hunk" },
+		{ "<leader>gv", ":lua require('gitsigns').select_hunk()<cr>", mode = { "n" }, desc = "Select Current Hunk" },
+
+		-- Staging and Unstaging
+		{ "<leader>gs", ":lua require('gitsigns').stage_hunk()<cr>", mode = { "n" }, desc = "Stage Hunk" },
+		{ "<leader>gS", ":lua require('gitsigns').stage_buffer()<cr>", mode = { "n" }, desc = "Stage Buffer" },
+		{ "<leader>gu", ":lua require('gitsigns').undo_stage_hunk()<cr>", mode = { "n" }, desc = "Unstage Hunk" },
+
+		-- Resetting
+		{ "<leader>gr", ":lua require('gitsigns').reset_hunk()<cr>", mode = { "n" }, desc = "Reset Hunk" },
+		{ "<leader>gR", ":lua require('gitsigns').reset_buffer()<cr>", mode = { "n" }, desc = "Reset Buffer" },
+
+		-- Previewing
+		{ "<leader>gp", ":lua require('gitsigns').preview_hunk()<cr>", mode = { "n" }, desc = "Preview Hunk" },
+		{ "<leader>gP", ":lua require('gitsigns').preview_hunk_inline()<cr>", mode = { "n" }, desc = "Preview Hunk in File" },
+
+		-- Viewing 
+		{ "<leader>gh", ":lua require('gitsigns').toggle_linehl()<cr>", mode = { "n" }, desc = "Toggle Line Hightlight" },
+		{ "<leader>gd", ":lua require('gitsigns').toggle_deleted()<cr>", mode = { "n" }, desc = "Toggle Deleted Hunks" },
+		{ "<leader>gw", ":lua require('gitsigns').toggle_word_diff()<cr>", mode = { "n" }, desc = "Toggle Word Diff" },
+		{ "<leader>gB", ":lua require('gitsigns').toggle_current_line_blame()<cr>", mode = { "n" }, desc = "Toggle Word Diff" },
+
+		-- Diff
+		{ "<leader>gD", ":lua require('gitsigns').diffthis()<cr>", mode = { "n" }, desc = "Diff File" },
+
+		-- Blame
+		{ "<leader>gb", ":lua require('gitsigns').blame_line({ full = true })<cr>", mode = { "n" }, desc = "Get Blame for Hunk" },
+	},
+
+	init = function()
+    require("which-key").register({
+      g = { name = "Git", },
+    }, { prefix = "<leader>", })
+	end,
+
+  config = function()
     local gs = require("gitsigns")
+		gs.setup()
     gs.toggle_numhl()
-
-    local wk = require("which-key")
-    wk.register({
-      g = {
-        name = "Git",
-
-        -- Navigation
-        j = { gs.next_hunk, "Go to Next Hunk" },
-        k = { gs.prev_hunk, "Go to Previous Hunk" },
-        v = { gs.select_hunk, "Select Current Hunk" },
-
-        -- Hunk Management
-        s = { gs.stage_hunk, "Stage Hunk" },
-        S = { gs.stage_buffer, "Stage Buffer" },
-        u = { gs.undo_stage_hunk, "Undo Stage Hunk" },
-        R = { gs.reset_hunk, "Reset Hunk" },
-
-        -- Preview
-        p = { gs.preview_hunk, "Preview Hunk" },
-        P = { gs.preview_hunk_inline, "Preview Hunk in File" },
-
-        -- View
-        h = { gs.toggle_linehl, "Toggle Line Highlight" },
-        d = { gs.toggle_deleted, "Toggle Deleted Hunks" },
-        w = { gs.toggle_word_diff, "Toggle Word Diff" },
-
-        -- Diff
-        D = { "<CMD>Neotree close<CR> <CMD>Gitsigns diffthis<CR>", "Diff File" },
-
-        b = {
-          function()
-            gs.blame_line({ full = true })
-          end,
-          "Get Blame for Hunk",
-        },
-      },
-    }, {
-      prefix = "<leader>",
-      buffer = nil,
-      silent = true,
-      noremap = true,
-      nowait = false,
-    })
   end,
 }

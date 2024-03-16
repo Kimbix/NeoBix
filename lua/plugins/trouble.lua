cc153c508084f118d5ca0af6f5cf578cfe5fa5fb
Kimbix <alemanhumberto106@gmail.com>
2024-03-15 21:53:47 -0400

    Trouble Modified

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,9 +1,12 @@
 return {
-	-- TODO: Configure keybinds
-	--
-	-- TODO: Configure Lazy Loading
 	"folke/trouble.nvim",
-	config = function()
-		require("trouble").setup()
-	end
+	lazy = true,
+
+	cmd = { "Trouble" },
+	keys = {
+		{ "<leader>tt", "<CMD>Trouble workspace_diagnostics<CR>", mode = { "n" }, desc = "Open window with workspace diagnostics" },
+		{ "<leader>tT", "<CMD>Trouble document_diagnostics<CR>", mode = { "n" }, desc = "Open window with document diagnostics" },
+		{ "<leader>tq", "<CMD>Trouble quickfix<CR>", mode = { "n" }, desc = "Open window with quickfix options" },
+	},
+	opts = { }
 }

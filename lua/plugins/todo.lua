8e0a2ee447f3405e8a539e8107ce68e6bb2c5a6e
Kimbix <alemanhumberto106@gmail.com>
2024-03-15 21:53:44 -0400

    Todo Modified

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -1,5 +1,13 @@
 return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {},
+
+	lazy = true,
+	event = "BufReadPre",
+	cmd = "TodoTelescope",
+	keys = {
+		{ "<leader>ft", "<CMD>TodoTelescope keywords=TODO,FIX<CR>", mode = { "n", "x" }, desc = "Live Grep" },
+	},
+
+	opts = {}
 }

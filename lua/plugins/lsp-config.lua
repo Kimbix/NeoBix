b99dafe66bd98f55c7df8e335ee64c762b39ff52
Kimbix <alemanhumberto106@gmail.com>
2024-03-17 16:23:11 -0400

    Lazy loaded lsp-config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -4,8 +4,8 @@ return {
 	-- For managing the lsps
 	{
 		"williamboman/mason.nvim",
-		lazy = true,
-		cmd  = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
+		lazy   = true,
+		cmd    = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
 		config = function()
 			require("mason").setup()
 		end,
@@ -19,10 +19,10 @@ return {
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = {
-					"lua_ls",       -- For Lua
-					"clangd",		    -- For C and C++
-					"omnisharp",    -- For C#
-					"tsserver",     -- For TypeScript
+					"lua_ls",  -- For Lua
+					"clangd",  -- For C and C++
+					"omnisharp", -- For C#
+					"tsserver", -- For TypeScript
 					"rust_analyzer" -- Rust Analyzer
 				},
 			})
@@ -36,6 +36,38 @@ return {
 
 		lazy = true,
 		event = { "BufReadPre", "BufNewFile" },
+		keys = {
+			{ "<leader>lK",     "<CMD>lua vim.lsp.buf.hover()<CR>",       mode = { "n", "x" }, desc = "Hover symbol" },
+			{ "<leader>lF",     "<CMD>lua vim.lsp.buf.format()<CR>",      mode = { "n", "x" }, desc = "Format document" },
+			{ "<leader>la",     "<CMD>lua vim.lsp.buf.code_action()<CR>", mode = { "n", "x" }, desc = "Code actions" },
+			{ "<leader>lR",     "<CMD>lua vim.lsp.buf.rename()<CR>",      mode = { "n", "x" }, desc = "Rename symbol" },
+			{ "<leader>ld",     "<CMD>Telescope lsp_definitions<CR>",     mode = { "n", "x" }, desc = "LSP definitions" },
+			{ "<leader>lt",     "<CMD>Telescope lsp_type_definitions",    mode = { "n", "x" }, desc = "LSP type definitions" },
+			{ "<leader>ls",     "<CMD>Telescope lsp_document_symbols",    mode = { "n", "x" }, desc = "LSP document symbols" },
+			{ "<leader>lS",     "<CMD>Telescope lsp_workspace_symbols",   mode = { "n", "x" }, desc = "LSP workspace symbols" },
+			{ "<leader>l<C-s>", "<CMD>Telescope lsp_global_symbols",      mode = { "n", "x" }, desc = "LSP global symbols" },
+			{ "<leader>li",     "<CMD>Telescope lsp_implementations",     mode = { "n", "x" }, desc = "LSP implementations" },
+			{ "<leader>lr",     "<CMD>Telescope lsp_references",          mode = { "n", "x" }, desc = "LSP references" },
+		},
+
+		-- TODO: Might add
+		-- A = { vim.lsp.buf.add_workspace_folder, "Add Workspace Folder" },
+		-- R = { vim.lsp.buf.remove_workspace_folder, "Remove Workspace Folder" },
+		-- L = {
+		-- 	function()
+		-- 		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		-- 	end,
+		-- 	"List Workspace Folders",
+		-- },
+
+		-- TODO: Might not re-add since trouble does this already
+		-- D = { builtin.diagnostics, "LSP Errors" },
+		
+		init = function()
+			require("which-key").register({
+				l = { name = "LSP", },
+			}, { mode = "n", prefix = "<leader>", })
+		end,
 
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -92,43 +124,6 @@ return {
 
 			lspconfig.gdscript.setup({
 				capabilities = capabilities,
-			})
-
-			-- Keybindings
-			local builtin = require("telescope.builtin")
-			local wk = require("which-key")
-			wk.register({
-				l = {
-					name = "LSP",
-					K = { vim.lsp.buf.hover, "Hover Symbol" },
-					F = { vim.lsp.buf.format, "Format Document" },
-					a = { vim.lsp.buf.code_action, "Code Actions" },
-					["<F2>"] = { vim.lsp.buf.rename, "Rename" },
-					A = { vim.lsp.buf.add_workspace_folder, "Add Workspace Folder" },
-					R = { vim.lsp.buf.remove_workspace_folder, "Remove Workspace Folder" },
-					L = {
-						function()
-							print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-						end,
-						"List Workspace Folders",
-					},
-
-					d = { builtin.lsp_definitions, "LSP Definitions" },
-					t = { builtin.lsp_type_definitions, "LSP Type Definitions" },
-					s = { builtin.lsp_document_symbols, "LSP Document Symbols" },
-					S = { builtin.lsp_workspace_symbols, "LSP Workspace Symbols" },
-					["C-s"] = { builtin.lsp_dynamic_workspace_symbols, "LSP Global Symbols" },
-					i = { builtin.lsp_implementations, "LSP Implementations" },
-					r = { builtin.lsp_references, "LSP References" },
-					D = { builtin.diagnostics, "LSP Errors" },
-				},
-			}, {
-				mode = "n",
-				prefix = "<leader>",
-				buffer = nil,
-				silent = true,
-				noremap = true,
-				nowait = false,
 			})
 		end,
 	},

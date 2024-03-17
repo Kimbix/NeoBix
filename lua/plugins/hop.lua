0e953643c3450336dac4bcfca222c341e643f369
Kimbix <alemanhumberto106@gmail.com>
2024-03-17 16:03:12 -0400

    Hop Lazy Loading

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,17 +1,10 @@
 return {
   "phaazon/hop.nvim",
-  config = function()
-    require("hop").setup({})
-    local wk = require("which-key")
-    wk.register({
-      f = { "<CMD>HopChar1<CR>", "Hop to Char" }
-    }, {
-      mode = "n",
-      prefix = "f",
-      buffer = nil,
-      silent = true,
-      noremap = true,
-      nowait = false,
-    })
-  end,
+	lazy = true,
+	cmd = { "Hop" },
+	keys = {
+		{ "ff", "<CMD>HopChar1<CR>", mode = { "n", "v" }, desc = "Hop to Char" },
+	},
+
+	opts = {},
 }
